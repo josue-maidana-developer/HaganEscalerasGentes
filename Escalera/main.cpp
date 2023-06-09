abcadbe625df4a2b30ade85f76702ccad7aa3db4
Josue Maidana <51282517+josue-maidana-developer@users.noreply.github.com>
2023-06-09 06:06:48 -0300

    Menu rapido

---
+++ Escalera/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    int opcion;
+    do {
+        cout << "=== Menu ===" << endl << endl;
+        cout << "1. Modo un jugador" << endl;
+        cout << "2. Modo dos jugadores" << endl;
+        cout << "3. Puntuacion maxima" << endl;
+        cout << "4. Salir" << endl << endl;
+        cout << "Ingrese una opcion: ";
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                ///Modo un jugador:
+                break;
+            case 2:
+                ///Modo dos jugadores:
+                break;
+            case 3:
+                ///Funcionalidad de puntuacion maxima
+                break;
+            case 4:
+                cout << "Game Over!" << endl;
+                break;
+            default:
+                cout << "Opcion no valida. Por favor, ingrese una opcion valida." << endl;
+        }
+        cout << endl;
+    } while (opcion != 4);
+
+    return 0;
+}
